dbedf8a91cb62cb483ee505bebac901fa070c0e0
Enock <enock.mokua@student.moringaschool.com>
2022-12-19 18:19:09 +0300

    create from for trial

---
+++ src/Pages/landing.js
@@ -0,0 +1,23 @@
+import './landing.css'
+
+export default function Landing() {
+  return (
+    <main id="landing">
+      <form id="user">
+        <span>Create User</span>
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="Username" />
+        <input type="number" placeholder="Total Amount" />
+        <input type="number" placeholder="Phone number" />
+      </form>
+
+      {/* <form id="">
+        <span>Create User</span>
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="Username" />
+        <input type="number" placeholder="Total Amount" />
+        <input type="number" placeholder="Phone number" />
+      </form> */}
+    </main>
+  )
+}
--- src/Pages/landing.jsx
+++
@@ -1,7 +0,0 @@
-import "./landing.css"
-
-export default function Landing(){
-    return <main id="landing">
-        Eloi eloi unataka namna gani
-    </main>
-}
