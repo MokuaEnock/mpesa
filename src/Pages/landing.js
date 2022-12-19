470ec91e9aea86284110fc3cc078c0fb150b6f16
Enock <enock.mokua@student.moringaschool.com>
2022-12-19 22:33:45 +0300

    grab form inputs

--- src/Pages/landing.js
+++ src/Pages/landing.js
@@ -7,14 +7,39 @@ export default function Landing() {
   let [amount, setAmount] = useState('')
   let [phone, setPhone] = useState('')
 
+  function handleSubmit() {}
+
   return (
     <main id="landing">
       <form id="user">
         <span>Create User</span>
-        <input type="email" placeholder="Email" />
-        <input type="text" placeholder="Username" />
-        <input type="number" placeholder="Total Amount" />
-        <input type="number" placeholder="Phone number" />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+
+        <input
+          type="number"
+          placeholder="Total Amount"
+          value={amount}
+          onChange={e => setAmount(e.target.value)}
+        />
+
+        <input
+          type="number"
+          placeholder="Phone number"
+          value={phone}
+          onChange={e => setPhone(e.target.value)}
+        />
 
         <button type="submit">Submit</button>
       </form>
