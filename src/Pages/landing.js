5473c9887732110cf97631362935e01abd23f17a
Enock <enock.mokua@student.moringaschool.com>
2022-12-25 20:43:10 +0300

    add fetch request fro daraja API

--- src/Pages/landing.js
+++ src/Pages/landing.js
@@ -70,6 +70,34 @@ export default function Landing() {
     })
   }
 
+  function handlePay() {
+    let headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+    headers.append('Authorization', 'Bearer GNAeMfYCpa3hZ0XLOC6RFEJHzaWf')
+
+    fetch('https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest', {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({
+        BusinessShortCode: 174379,
+        Password:
+          'MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjIxMjI1MjAzODA3',
+        Timestamp: '20221225203807',
+        TransactionType: 'CustomerPayBillOnline',
+        Amount: 1,
+        PartyA: 254717594017,
+        PartyB: 174379,
+        PhoneNumber: 254717594017,
+        CallBackURL: 'https://mydomain.com/path',
+        AccountReference: 'CompanyXLTD',
+        TransactionDesc: 'Payment of X',
+      }),
+    })
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log(error))
+  }
+
   return (
     <main id="landing">
       <form id="user" onSubmit={handleSubmit}>
